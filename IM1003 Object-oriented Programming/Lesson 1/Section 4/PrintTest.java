02ea43fea142909ca7af7b183493425f65dfa36d
boyshawn <boyshawn@hotmail.com>
2015-09-06 11:19:48 +0800

    Complete Exercise in Lesson 1 Section 4. Rename exercise to PrintTest.java instead of exercise_1.4 to be more compliant of Java naming conventions.

---
+++ IM1003 Object-oriented Programming/Lesson 1/Section 4/PrintTest.java
@@ -0,0 +1,25 @@
+public class PrintTest{
+	public static void main(String[] args){
+        //Print alternate rows of asterisk
+        System.out.println("* * * * * ");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * ");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * ");
+
+        //Print a rectangular of asterisk
+        System.out.println("* * * * *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("* * * * *");
+
+        //Print a triangular of asterisk
+        System.out.println("* * * * *");
+        System.out.println(" *     * ");
+        System.out.println("  *   *  ");
+        System.out.println("   * *   ");
+        System.out.println("    *    ");
+
+	}
+}
--- IM1003 Object-oriented Programming/Lesson 1/exercise_1.4.java
+++
@@ -1,5 +0,0 @@
-public class Exercise_1_4{
-	public static void main(String[] args){
-
-	}
-}
