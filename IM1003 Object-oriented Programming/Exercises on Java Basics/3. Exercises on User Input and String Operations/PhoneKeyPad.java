a925851d5b1a8258b459f16c60ad001f56da5d33
boyshawn <boyshawn@hotmail.com>
2015-10-04 02:01:25 +0800

    Finish Phone Key Pad exercise and Reverse String exercise

--- IM1003 Object-oriented Programming/Exercises on Java Basics/3. Exercises on User Input and String Operations/PhoneKeyPad.java
+++ IM1003 Object-oriented Programming/Exercises on Java Basics/3. Exercises on User Input and String Operations/PhoneKeyPad.java
@@ -1,10 +1,82 @@
 /*
- * Exercise PhoneKeyPad: On your phone keypad, the alphabets are mapped to digits as follows: ABC(2), DEF(3), GHI(4), JKL(5), MNO(6), PQRS(7), TUV(8), WXYZ(9).
+ * Exercise PhoneKeyPad: On your phone keypad, the alphabets are mapped to 
+ * digits as follows: ABC(2), DEF(3), GHI(4), JKL(5), MNO(6), PQRS(7), TUV(8), 
+ * WXYZ(9).
  *
- * Write a program called PhoneKeyPad, which prompts user for a String (case insensitive), and converts to a sequence of Keypad digits. Use a nested-if (or switch-case) in this exercise. Modify your program to use an array for table look-up later.
+ * Write a program called PhoneKeyPad, which prompts user for a String (case 
+ * insensitive), and converts to a sequence of Keypad digits. Use a nested-if 
+ * (or switch-case) in this exercise. Modify your program to use an array for 
+ * table look-up later.
  *
- * Hints: You can use in.next().toLowerCase() to read a string and convert it to lowercase to reduce your cases.
+ * Hints: You can use in.next().toLowerCase() to read a string and convert it 
+ * to lowercase to reduce your cases.
  * @author  Francis Pang
  * @since   %I% %G%
  * @version 1.0
  */
+
+import java.util.Scanner;
+
+public class PhoneKeyPad{
+    public static void main(String[] args){
+        Scanner keyboardInput = new Scanner(System.in);
+        String keypadSequence;
+        String digitSequence = new String();
+
+        //Asking for input
+        System.out.print("Enter a string: ");
+        keypadSequence = keyboardInput.next().toLowerCase();
+
+        //Using Switch Case to determine output
+        for(int i = 0;i < keypadSequence.length(); i++){
+            switch(keypadSequence.charAt(i)){
+                case 'a': case 'b': case 'c':
+                    digitSequence = digitSequence.concat("2");
+                    break;
+                case 'd': case 'e': case 'f':
+                    digitSequence = digitSequence.concat("3");
+                    break;
+                case 'g': case 'h': case 'i':
+                    digitSequence = digitSequence.concat("4");
+                    break;
+                case 'j': case 'k': case 'l':
+                    digitSequence = digitSequence.concat("5");
+                    break;
+                case 'm': case 'n': case 'o':
+                    digitSequence = digitSequence.concat("6");
+                    break;
+                case 'p': case 'q': case 'r': case 's':
+                    digitSequence = digitSequence.concat("7");
+                    break;
+                case 't': case 'u': case 'v':
+                    digitSequence = digitSequence.concat("8");
+                    break;
+                case 'w': case 'x': case 'y': case 'z':
+                    digitSequence = digitSequence.concat("9");
+                    break;
+            }
+        }
+
+        //Printing result
+        System.out.print("[Switch Case] The phone number you have key in ");
+        System.out.println("is " + digitSequence);
+
+        //Using Array lookup table to determine output
+        digitSequence = new String();
+        char[][]phonePad = { 
+            {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'},
+            {'2', '2', '2', '3', '3', '3', '4', '4', '4', '5', '5', '5', '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '9', '9', '9', '9'}
+        };
+
+        for(int j = 0; j < keypadSequence.length();j++){
+            for(int i = 0;i < phonePad[0].length; i++){
+                if(keypadSequence.charAt(j) == phonePad[0][i]){
+                    digitSequence = digitSequence.concat(Character.toString(phonePad[1][i])); //Type cast Character to string
+                }
+            }
+        }
+        //Printing result
+        System.out.print("[Switch Case] The phone number you have key in ");
+        System.out.println("is " + digitSequence);
+    }
+}
--- IM1003 Object-oriented Programming/Exercises on Java Basics/3. Exercises on User Input and String Operations/ReverseString.java
+++ IM1003 Object-oriented Programming/Exercises on Java Basics/3. Exercises on User Input and String Operations/ReverseString.java
@@ -1,5 +1,9 @@
 /*
- * Exercise ReverseString: Write a program called ReverseString, which prompts user for a String, and prints the reverse of the String. The output shall look like:
+ * Exercise ReverseString: Write a program called ReverseString, which prompts 
+ * user for a String, and prints the reverse of the String. The output shall 
+ * look like:
+ * Enter a String: <b>abcdef</b>
+ * The reverse of String "abcdef" is "fedcba".
  * @author  Francis Pang
  * @since   %I% %G%
  * @version 1.0
@@ -7,16 +11,20 @@
 
 import java.util.Scanner;
 public class ReverseString {
-   public static void main(String[] args) {
-      String inStr;        // input String
-      int inStrLen;        // length of the input String
-   
-      Scanner in = new Scanner(System.in);
-      System.out.print("Enter a String: ");
-      inStr = in.next();   // use next() to read String
-      inStrLen = inStr.length();
-   
-      // Use inStr.charAt(index) to extract character at 'index' from inStr
-      ......
-   }
+    public static void main(String[] args) {
+        String inStr;        // input String
+        int inStrLen;        // length of the input String
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        inStr = in.next();   // use next() to read String
+        inStrLen = inStr.length();
+
+        // Use inStr.charAt(index) to extract character at 'index' from inStr
+        System.out.print("The reverse of String \"" + inStr + "\" is \"");
+        for(int i = inStrLen - 1; i >= 0; i--){
+            System.out.print(inStr.charAt(i));
+        }
+        System.out.println("\".");
+    }
 }
