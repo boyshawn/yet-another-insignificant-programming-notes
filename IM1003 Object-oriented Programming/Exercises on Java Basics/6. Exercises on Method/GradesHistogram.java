1f1acd5fe36bb9f0cd571ac771488862fa5d64d5
Francis Pang <boyshawn@hotmail.com>
2015-10-20 14:29:24 +0800

    Update exercise 6

--- IM1003 Object-oriented Programming/Exercises on Java Basics/6. Exercises on Method/GradesHistogram.java
+++ IM1003 Object-oriented Programming/Exercises on Java Basics/6. Exercises on Method/GradesHistogram.java
@@ -8,30 +8,94 @@
  * @version 1.0
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class GradesHistogram {
-   public static int[] grades;
-      // Declare an int array of grades, to be allocated later
-   public static int[] bins = new int[10];
-      // Declare and allocate an int array for histogram bins.
-      // 10 bins for 0-9, 10-19,...., 90-100
-   
-   public static void main(String[] args) {
-      readGrades("grades.in");
-      computeHistogram();
-      printHistogramHorizontal();
-      printHistogramVertical();
-   }
-   
-   // Read the grades from "filename", store in "grades" array.
-   // Assume that the inputs are valid.
-   public static void readGrades(String filename) { ...... }
+	public static int[] grades;
+	// Declare an int array of grades, to be allocated later
+	public static int[] bins = new int[10];
+	// Declare and allocate an int array for histogram bins.
+	// 10 bins for 0-9, 10-19,...., 90-100
 
-   // Based on "grades" array, populate the "bins" array.
-   public static void computeHistogram() { ....... }
+	public static void main(String[] args) {
+		readGrades("grades.in");
+		computeHistogram();
+		printHistogramHorizontal();
+		printHistogramVertical();
+	}
 
-   // Print histogram based on the "bins" array.
-   public static void printHistogramHorizontal() { ...... }
+	// Read the grades from "filename", store in "grades" array.
+	// Assume that the inputs are valid.
+	public static void readGrades(String filename) {
+		Scanner fileInput;
+		int numberOfStudent;
 
-   // Print histogram based on the "bins" array.
-   public static void printHistogramVertical() { ...... }
+		try {
+			fileInput = new Scanner(new File(filename));
+
+			//Getting user input
+			numberOfStudent = fileInput.nextInt();
+			grades = new int[numberOfStudent];
+
+			for(int i =0; i < numberOfStudent; i++){
+				grades[i] = fileInput.nextInt();
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (InputMismatchException e){
+			// TODO Auto-generated catch block
+			System.out.println("There is a non-numeric word in the file.");
+		}
+	}
+
+	// Based on "grades" array, populate the "bins" array.
+	public static void computeHistogram() { 
+		//Initialise the array
+		for(int bin : bins){
+			bin = 0;
+		}
+		for(int grade : grades){
+			if(grade == 100){	//To handle the exception of 100 marks
+				bins[9]++;
+			}
+			else{
+				bins[(grade / 10)]++;
+			}
+		}
+	}
+
+	// Print histogram based on the "bins" array.
+	public static void printHistogramHorizontal() { 
+		for(int i = 0; i < bins.length; i++){
+			if(i == 0){	//Add in the extra space for the 0 - 9 
+				System.out.print(" " + i * 10 + " -   " + (((i + 1)* 10) - 1) 
+						+ ": ");
+			}
+			else if (i == bins.length){
+				System.out.print(i * 10 + " - " + ((i + 1)* 10) + ": "); 
+			}
+			else{
+				System.out.print(i * 10 + " -  " + (((i + 1)* 10) - 1) + ": ");				
+			}
+
+			int j = bins[i];
+			while(j >= 0){
+				System.out.print("*");
+				j--;
+			}
+
+			System.out.println();
+		}
+	}
+
+	// Print histogram based on the "bins" array.
+	public static void printHistogramVertical() { 
+		
+	}
 }
